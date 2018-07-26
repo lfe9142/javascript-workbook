d7d59c64fc583027c17084d2101bafe00d1885af
lfe9142 <lflippo001@gmail.com>
2018-07-25 23:10:49 -0500

    finished piglatin project

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -7,11 +7,69 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+//Whiteboard/Plan:
+//take a word
+//turn word into array with split()
+//if its a vowel add 'yay'
+//if its not a vowel:
+//put the first letter at the end, then add 'ay'
+//turn array into string with join()
+
+
+//isInputValid(word);
+//will check if it isnt a number and isnt empty
+const isValidInput=(word)=> word && !Number(word);
+
+
+//lowercaseAndTrimInput(word);
+//use trim() and toLowerCase()
+//returns string that is trim and lowercase
+const lowercaseAndTrimString=(word)=> word.trim().toLowerCase();
+
+
+//isLetterVowel(letter)
+//returns true if letter equals a e i o or u
+const isLetterVowel=(letter)=> letter == 'a' || letter == 'e' ||  letter == 'i' || letter == 'o' || letter == 'u';
+
+//putLetterOnEnd(wordArray)
+//loop though wordArray until we come to a vowel
+//for each letter 
+//use array.length() and array.slice()
+const putLeadingconsonantsAtEnd=(wordArray)=> {
+  let leadingConsonants = [];
+  let currentIndex = 0;
+  for(currentIndex = 0; currentIndex < wordArray.length; currentIndex++) {
+    let currentLetter = wordArray[currentIndex];
+    console.log(currentLetter);
+    if(isLetterVowel(currentLetter)) {
+      break;
+    }
+
+    leadingConsonants.push(currentLetter);
+  }
+
+  const trailingWord = wordArray.slice(currentIndex, wordArray.length);
+  return trailingWord.concat(leadingConsonants);
+}
+
+//addAYToEnd(wordArray)
+const addAYToEnd=(wordArray)=> wordArray.join('') + "ay";
+//addYAYToEnd(wordArray)
+//accepts a wordArray and returns a string with YAY appended to the end
+const addYAYToEnd=(wordArray)=> wordArray.join('') + "yay";
+
 
 function pigLatin(word) {
-
   // Your code here
+  if(isValidInput(word)) {
+    let wordArray = lowercaseAndTrimString(word).split('');
 
+    if(isLetterVowel(wordArray[0])) {
+      return addYAYToEnd(wordArray);
+    }
+
+    return addAYToEnd(putLeadingconsonantsAtEnd(wordArray));
+  }
 }
 
 
