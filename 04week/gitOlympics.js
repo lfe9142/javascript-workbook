d0fb995941d6c7b0e809f9dd3f363e7979b43a37
lfe9142 <lflippo001@gmail.com>
2018-08-02 19:16:19 -0500

    commit 2

--- 04week/gitOlympics.js
+++ 04week/gitOlympics.js
@@ -4,6 +4,6 @@ const printListOfOlympians=(olympicArr)=> {
         console.log(element);
     });
 }
-
+//comment 1s
 const testArr = ["nameOne", "nameTwo", "namethree", "name4", "name4"];
 printListOfOlympians(testArr);
