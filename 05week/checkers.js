fe9152b946b0e658f803c75db58ebf8f413d19db
lfe9142 <lflippo001@gmail.com>
2018-08-21 00:21:54 -0500

    started implemtation for isValidMOve method

--- 05week/checkers.js
+++ 05week/checkers.js
@@ -85,10 +85,7 @@ class Board {
 //functions/methods we need
 //moveChecker(fromLocation, toLocation)
 
-//isValidMove(fromLocation, toLocation)
-//cant move to "white spaces" on board
-//cant move diagonal if next next space is taken
-//cant move out of the grid
+
 
 //win condition
 //tie condition
@@ -126,6 +123,60 @@ class Game {
     this.board.grid[whereRow][whereColumn] = this.board.grid[whichRow][whichColumn];
     this.board.grid[whichRow][whichColumn] = null;
   }
+
+  //isValidMove(fromLocation, toLocation)
+  //cant move to "white spaces" on board
+  //cant move diagonal if next next space is taken
+  //cant move out of the grid
+  //returns true if the conditions above are true.
+  isValidMove(whichPiece, toWhere) {
+    const whichArr = whichPiece.split('');
+    const whichRow = parseInt(whichArr[0], 10);
+    const whichColumn = parseInt(whichArr[1], 10);
+
+
+    const toWhereArr = toWhere.split('');
+    const toWhereRow = parseInt(toWhereArr[0], 10);
+    const toWhereColumn = parseInt(toWhereArr[1], 10);
+
+
+    //move is out of range
+    if(!this.isMoveInRange(whichRow, whichColumn) || !this.isMoveInRange(toWhereRow, toWhereColumn)) {
+      console.log("** out of range");
+      return false;
+    }
+
+    //move is on a 'white' space
+    if(!this.isMoveOnBlackSpace(toWhereRow, toWhereColumn)) {
+      console.log("**move no on black space")
+      return false;
+    }
+
+    //move space is empty
+    if(this.board.grid[toWhereRow][toWhereColumn] != null) {
+      console.log("**move space taken");
+      return false;
+    }
+
+    return true;
+  }
+
+  //returns true if number at 0 and 1 are both in between 0 and 7 (inclusive)
+  isMoveInRange(row, column) {
+    return (row >= 0 && row <= 7) && (column >= 0 && column <= 7)
+  }
+
+  isMoveOnBlackSpace(row, column) {
+    return (row + column) %  2 != 0;
+  }
+
+  //helper method that takes a string containing a coordanate and returns an array of numbers
+  coordStringToArray(coordanate) {
+    const coordArr = coordanate.split('')
+    coordArr[0] = parseInt(coordArr[0], 10);
+    coordArr[1] = parseInt(coordArr[1], 10);
+    return coordArr;
+  }
 }
 
 //Class that will repersent the pieces, contains a symbol that will be set to X or O
@@ -148,7 +199,8 @@ function getPrompt() {
 
 const game = new Game();
 game.start();
-game.moveChecker('01', '44');
+//game.moveChecker('01', '44');
+console.log(game.isValidMove('01', '21'));
 
 
 // Tests
